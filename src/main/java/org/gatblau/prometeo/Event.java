75a81fcc2f58dffe9f5568aa96cce5f1dca48f9b
gatblau <andres.alos.m@gmail.com>
2017-11-11 08:39:57 +0000

    Makes errors an attribute of a recorded event instead of a separate event on its own.

--- src/main/java/org/gatblau/prometeo/Event.java
+++ src/main/java/org/gatblau/prometeo/Event.java
@@ -10,17 +10,27 @@ public class Event {
     private String _projectCode;
     private String _date;
     private String _eventType;
+    private boolean _success;
     private String _info;
     private String _command;
+    private String _error;
 
     public Event() {
     }
 
     public Event(String processId, String projectCode, EventType eventType, String info){
-        this(processId, projectCode, eventType, info, "none");
+        this(processId, projectCode, eventType, info, null, true);
     }
 
-    public Event(String processId, String projectCode, EventType eventType, String info, String command){
+    public Event(String processId, String projectCode, EventType eventType, String info, boolean success, String error){
+        this(processId, projectCode, eventType, info, null, success, error);
+    }
+
+    public Event(String processId, String projectCode, EventType eventType, String info, String command, boolean success) {
+        this(processId, projectCode, eventType, info, null, success, null);
+    }
+
+    public Event(String processId, String projectCode, EventType eventType, String info, String command, boolean success, String error){
         _eventId = UUID.randomUUID().toString();
         _projectCode = projectCode;
         _processId = processId;
@@ -28,10 +38,16 @@ public class Event {
         _eventType = eventType.name();
         _info = info;
         _command = command;
+        _success = success;
+        _error = error;
     }
 
     public String getEventId() {
         return _eventId;
+    }
+
+    public void setEventId(String eventId) {
+        _eventId = eventId;
     }
 
     public String getProjectCode() {
@@ -40,10 +56,6 @@ public class Event {
 
     public void setProjectCode(String value) {
         _projectCode = value;
-    }
-
-    public void setEventId(String eventId) {
-        _eventId = eventId;
     }
 
     public String getProcessId() {
@@ -91,5 +103,21 @@ public class Event {
         Date now = new Date();
         String strDate = sdfDate.format(now);
         return strDate;
+    }
+
+    public boolean getSuccess() {
+        return _success;
+    }
+
+    public void setSuccess(boolean success) {
+        _success = success;
+    }
+
+    public String getError() {
+        return _error;
+    }
+
+    public void setError(String error) {
+        _error = error;
     }
 }
